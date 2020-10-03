d1c8d491274f17ce41bfc77fd94b91902473a653
Henrique Campos <henriiquecampos@gmail.com>
2020-10-03 11:45:36 -0300

    Refactor TowerPlacement
    Fix TowerPlacement placing BasicTowers on occupied cells. Also improve UX when trying to place towers on unplaceabe cells

--- source/Levels/TowerPlacement/TowerPlacement.gd
+++ source/Levels/TowerPlacement/TowerPlacement.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
+# The ID of _grid cells that BasicTower can be placed on
 const PLACEBLE_CELLS_ID := 0
 
-export var tower_placement_offset := Vector2(32, 64)
-
 onready var _grid := $Grid
+onready var _tower_placement_offset: Vector2 = _grid.cell_size
 
-var _current_tower: Node2D
+var _current_tower: BasicTower
 
 
 func _ready() -> void:
+	_tower_placement_offset.x *= 0.5
 	set_process(false)
 
 
@@ -18,13 +19,19 @@ func _process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("tower_placement") and is_processing():
+		get_tree().set_input_as_handled()
 	if event.is_action_released("tower_placement"):
-		set_process(false)
-		_grid.visible = false
+		_place_tower()
 
 
 func add_new_tower(tower_scene: PackedScene) -> void:
 	var tower_instance := tower_scene.instance() as Node2D
+	if Player.current_gold - tower_instance.cost < 0:
+		tower_instance.queue_free()
+		return
+	Player.current_gold -= tower_instance.cost
+
 	add_child(tower_instance)
 
 	_current_tower = tower_instance
@@ -33,9 +40,26 @@ func add_new_tower(tower_scene: PackedScene) -> void:
 	_grid.visible = true
 
 
+func set_cell_unplaceable(cell: Vector2) -> void:
+	_grid.set_cellv(cell, -1)
+
+
+func _place_tower() -> void:
+	var cell: Vector2 = _grid.world_to_map(get_global_mouse_position())
+	if not cell in _grid.get_used_cells_by_id(PLACEBLE_CELLS_ID):
+		Player.current_gold += _current_tower.cost
+		_current_tower.queue_free()
+	else:
+		set_cell_unplaceable(cell)
+	set_process(false)
+	_grid.visible = false
+
+
 func _snap_tower_to_grid() -> void:
 	var cell: Vector2 = _grid.world_to_map(get_global_mouse_position())
 	if not cell in _grid.get_used_cells_by_id(PLACEBLE_CELLS_ID):
-		return
+		_current_tower.modulate = Color(1, 0.375, 0.375)
+	else:
+		_current_tower.modulate = Color.white
 	_current_tower.global_position = _grid.map_to_world(cell)
-	_current_tower.global_position += tower_placement_offset
+	_current_tower.global_position += _tower_placement_offset
