6008d62ef79509610719ea82066158cecb96bd5f
Henrique Campos <henriiquecampos@gmail.com>
2020-10-21 20:25:59 -0300

    Add RangePreview to BasicTower

--- source/Actors/Towers/BasicTower.gd
+++ source/Actors/Towers/BasicTower.gd
@@ -8,6 +8,11 @@ export var cost := 100
 onready var _label := $Label
 onready var _weapon := $Weapon2D
 onready var _sell_button := $SellButton
+onready var _range_preview := $RangePreview
+
+func _ready() -> void:
+	_range_preview.radius = _weapon.sight_range
+	_range_preview.show_range()
 
 
 func _on_SelectableArea2D_selection_changed(selected) -> void:
@@ -18,3 +23,12 @@ func _on_SelectableArea2D_selection_changed(selected) -> void:
 func _on_SellButton_pressed() -> void:
 	emit_signal("sold", cost * 0.5, position)
 	queue_free()
+
+
+func _on_SelectableArea2D_mouse_entered() -> void:
+	_range_preview.radius = _weapon.sight_range
+	_range_preview.show_range()
+
+
+func _on_SelectableArea2D_mouse_exited() -> void:
+	_range_preview.hide_range()
---
+++ source/Actors/Towers/RangePreview/RangePreview.gd
@@ -0,0 +1,31 @@
+extends Sprite
+
+export var radius := 100.0
+export var tween_duration := 0.5
+
+onready var _tween := $Tween
+
+
+func show_range() -> void:
+	var ratio = radius / texture.get_width()
+	var final_scale = Vector2(ratio, ratio)
+	
+	if _tween.is_active():
+		_tween.stop_all()
+	
+	_tween.interpolate_property(self, "scale", scale, final_scale,
+			tween_duration, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	_tween.interpolate_property(self, "modulate", modulate, Color(1, 1, 1, 1),
+			tween_duration, Tween.TRANS_LINEAR)
+	_tween.start()
+
+
+func hide_range() -> void:
+	if _tween.is_active():
+		_tween.stop_all()
+	
+	_tween.interpolate_property(self, "scale", scale, Vector2.ZERO,
+			tween_duration, Tween.TRANS_BACK, Tween.EASE_IN)
+	_tween.interpolate_property(self, "modulate", modulate, Color(1, 1, 1, 0),
+			tween_duration * 2.0, Tween.TRANS_LINEAR)
+	_tween.start()
