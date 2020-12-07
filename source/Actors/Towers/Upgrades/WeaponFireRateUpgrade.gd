2e8e910cc9e53aa3d3d19cf04538e50e13d0b738
Nathan Lovato <nathan@gdquest.com>
2020-12-07 10:33:28 -0600

    Fix wrong property name in WeaponFireRateUpgrade

--- source/Actors/Towers/Upgrades/WeaponFireRateUpgrade.gd
+++ source/Actors/Towers/Upgrades/WeaponFireRateUpgrade.gd
@@ -3,4 +3,4 @@ extends Upgrade
 
 func _apply_upgrade() -> void:
 	# The maximum allowed fire rate is of 20 bullets per second.
-	weapon.cooldown = max(0.05, weapon.cooldown - value)
+	weapon.fire_cooldown = max(0.05, weapon.fire_cooldown - value)
