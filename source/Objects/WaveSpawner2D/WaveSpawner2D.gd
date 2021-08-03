ac39b23c638687b8a5e6d635fff4c041c011b6c8
johnnygossdev <me@johnnygoss.dev>
2021-08-03 16:28:35 +0100

    Fix: Rename spawning variable to match course

--- source/Objects/WaveSpawner2D/WaveSpawner2D.gd
+++ source/Objects/WaveSpawner2D/WaveSpawner2D.gd
@@ -8,11 +8,11 @@ export var spawn_scene: PackedScene
 
 
 func spawn() -> Node2D:
-	var spawning: Node2D = spawn_scene.instance()
+	var spawn: Node2D = spawn_scene.instance()
 
-	add_child(spawning)
-	spawning.set_as_toplevel(true)
-	spawning.global_position = global_position
+	add_child(spawn)
+	spawn.set_as_toplevel(true)
+	spawn.global_position = global_position
 
-	emit_signal("spawned", spawning)
-	return spawning
+	emit_signal("spawned", spawn)
+	return spawn
